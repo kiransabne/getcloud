6a15289d37bb1ed04ca5c9248519521a802adf9d
kiran sabne <kiran@instagetit.in>
2018-01-04 14:23:38 +0000

    online-reputation-page

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get 'pages/hosting' => 'high_voltage/pages#show', id: 'hosting'
   get 'pages/contactus' => 'high_voltage/pages#show', id: 'contactus'
   get 'pages/web-development-services' => 'high_voltage/pages#show', id: 'web-development-services'
+  get 'pages/online-reputation-management' => 'high_voltage/pages#show', id: 'online-reputation-management'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
